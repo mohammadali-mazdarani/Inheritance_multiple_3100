95d71c24971b66539b722705570a661c73d31d02
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2025-03-20 23:48:59 -0700

    Build : Build Practice.cpp file

---
+++ Practice.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+
+/* 
+* Description of program
+*/
+
+/*
+* Branch name
+*
+*
+*/
+
+// TODO Classes
+//-------------------
+
+int main(int argc, char** argv)
+{
+
+getch();
+}
+
+// TODO Method's for classes
+//--------------------------

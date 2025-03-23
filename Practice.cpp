8af93b76eae216c163091172911a03279f2e6846
Mohammad ali mazdarani <74422713+mohammadali-mazdarani@users.noreply.github.com>
2025-03-22 19:41:01 -0700

    Build : Build body of method_1_pubic_class_b

--- Practice.cpp
+++ Practice.cpp
@@ -53,7 +53,7 @@ getch();
 void class_b :: method_1_public_class_b_set_number_1_protected_class_b( int number_1_input_method_1_protected_class_b) {
 
 
-
+number_1_protected_class_b = number_1_input_method_1_protected_class_b ;
 
 } ;
 //--------------------------
